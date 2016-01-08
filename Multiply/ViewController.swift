2c0f6e73c1a046e6e75b00d59bf962dea2be3297
HChlebek <student@HChlebek.local>
2016-01-08 11:16:11 -0600

    initial commit

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var productLabel: UILabel!
+    
+    @IBOutlet weak var marioImage: UIImageView!
 
-    override func viewDidLoad() {
+    @IBOutlet weak var segmentController: UISegmentedControl!
+
+    override func viewDidLoad()
+    
+    
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func Button1(sender: AnyObject)
+    {
+    let inputOne = (textField1.text! as NSString).integerValue
+    let inputTwo = (textField2.text! as NSString).integerValue
+        
+    var answer = inputOne * inputTwo
+        
+    productLabel.text = "\(answer)"
+   
+    if answer == 64
+    { marioImage.image = UIImage (named: "mario")
+        }
+     
+    else if answer % 2 == 1
+    { marioImage.image = UIImage (named: "funny-animals-pictures-5")
+        }
 
+    else if answer % 2 == 0
+    { marioImage.image = UIImage (named: "these_funny_animals_1099_640_01")
+        }
+        
+    
+        
+    if (segmentController.selectedSegmentIndex) == 0
+    { answer = inputOne + inputTwo
+        }
+        
+    if (segmentController.selectedSegmentIndex) == 1
+    { answer = inputOne - inputTwo
+        }
 
+    if (segmentController.selectedSegmentIndex) == 2
+    { answer = inputOne * inputTwo
+        }
+        
+    if (segmentController.selectedSegmentIndex) == 3
+    { answer = inputOne / inputTwo
+        }
+        productLabel.text = "\(answer)"
 }
+}
+
+
+
 
